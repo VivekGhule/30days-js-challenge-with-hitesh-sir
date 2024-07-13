4a52253c8186c49540204d52cb1b0363640007ed
VivekGhule <vivekghule777@gmail.com>
2024-07-13 16:53:58 +0530

    Day01 Complete

---
+++ Day 01 Challenge/Day01.js
@@ -0,0 +1,67 @@
+
+// Activity 1: 
+
+// TASK 1
+
+var num = 777;
+console.log(num);
+
+// TASK 2
+
+let name = "Vivek";
+console.log(name);
+
+
+
+// Activity 2: 
+
+// Task 3
+const isFriends = true;
+console.log(isFriends);
+
+// Activity 3: 
+
+// Task 4
+
+const luckChamp = 777;
+const Country= "India"
+const isPracrice = true
+const data ={
+    name:"Vivek",
+    age:85,
+    profession:"Student",
+}
+
+const arr =[1,2,3,4,5,"Chai"]
+
+console.log(typeof luckChamp)
+console.log(typeof Country)
+console.log(typeof isPracrice)
+console.log(typeof data)
+console.log(typeof arr)
+
+// Activity 4: 
+
+             // Task 5
+    let task5 = 776;
+    console.log(task5);
+
+    task5 = 777;
+    console.log(task5);
+   
+
+
+// Activity 5:
+
+    // Task 6
+
+    const check = "Demo";
+    console.log(check);
+
+    // check ="Demo2"
+    // console.log(check);
+
+    // output : Assignment to constant variable.  we can not reassigning value with const declared variable
+
+
+
